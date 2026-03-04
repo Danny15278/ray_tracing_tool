92d4f961f1c5f54e1b39a741c31c6edb3c367712
Danny15278 <danny26574@gmail.com>
2026-03-05 11:49:31 +1300

    calculations for random diffuse vectors

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "vec3.h"
+
 #include <random>
 
 inline double random_double() {
@@ -13,5 +15,26 @@ inline double random_double() {
 inline double random_double(double min, double max) {
 	return min + (max - min) * random_double();
 }
+
+
+inline Vec3 random_vec3() {
+	return Vec3(random_double(), random_double(), random_double());
+}
+
+inline Vec3 random_vec3(double min, double max) {
+	return Vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+}
+
+// Check if random vector falls inside sphere (for diffuse objects)
+
+inline Vec3 random_unit_vector() {
+        while (true) {
+                Vec3 point{ random_vec3(-1, 1) };      
+                double squared{ point.length_squared() };
+                if (squared <= 1 && squared >= 1e-160)
+                        return point / std::sqrt(squared);
+        }
+}
+
 
 #endif
--- src/vec3.h
+++ src/vec3.h
@@ -12,56 +12,55 @@
 
 class Vec3 {
 	
-	public:
-		double x, y, z;
+public:
+	double x, y, z;
 
-		Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
-		Vec3() : x(0), y(0), z(0) {}
+	Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
+	Vec3() : x(0), y(0), z(0) {}
 
 
 
-		Vec3 operator+(const Vec3& other) const {
-			return Vec3(x + other.x, y + other.y, z + other.z);
-		}
+	Vec3 operator+(const Vec3& other) const {
+		return Vec3(x + other.x, y + other.y, z + other.z);
+	}
 
-		Vec3 operator-(const Vec3& other) const {
-			return Vec3(x - other.x, y - other.y, z - other.z);
-		}
+	Vec3 operator-(const Vec3& other) const {
+		return Vec3(x - other.x, y - other.y, z - other.z);
+	}
 
-		Vec3 operator*(const double scalar) const {
-			return Vec3(x * scalar, y * scalar, z * scalar); 
-		}
+	Vec3 operator*(const double scalar) const {
+		return Vec3(x * scalar, y * scalar, z * scalar); 
+	}
 
-		Vec3& operator+=(const Vec3& other) {
-			x += other.x;
-			y += other.y;
-			z += other.z;
-			return *this;
-		}
+	Vec3& operator+=(const Vec3& other) {
+		x += other.x;
+		y += other.y;
+		z += other.z;
+		return *this;
+	}
 
-		Vec3 operator/(const double scalar) const {
-			return Vec3(x * (1/scalar), y * (1/scalar), z * (1/scalar));
-		}
+	Vec3 operator/(const double scalar) const {
+		return Vec3(x * (1/scalar), y * (1/scalar), z * (1/scalar));
+	}
 
-		
+	
+	double length_squared() const {
+		return x * x + y * y + z * z;
+	}
 
-		double length_squared() const {
-			return x * x + y * y + z * z;
-		}
+	double length() const {
+		return std::sqrt(length_squared());
+	}
+	
+	Vec3 operator+(const double scalar) const {
+		return Vec3(x + scalar, y + scalar, z + scalar);
+	}
+	
+	Vec3 normalised() const {
+		double len = length();
+		return Vec3(x / len, y / len, z / len);
+	}
 
-		double length() const {
-			return std::sqrt(length_squared());
-		}
-		
-		Vec3 operator+(const double scalar) const {
-			return Vec3(x + scalar, y + scalar, z + scalar);
-		}
-		
-		Vec3 normalised() const {
-			double len = length();
-			return Vec3(x / len, y / len, z / len);
-		}	
-		
 };
 
 Vec3 operator*(const double scalar, const Vec3& v) {
@@ -75,4 +74,5 @@ double operator*(const Vec3& v1, const Vec3& v2) {
 inline double dot_product(const Vec3& v1, const Vec3 v2) {
 	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
 }
+
 #endif
